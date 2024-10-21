13a4e4535a5205ccf214ac288aca4b6337febacf
LeticiaNunes <leticianunesq8@gmail.com>
2024-10-20 23:55:33 -0300

    inicializando projeto spring boot

---
+++ src/main/java/tde3/pedido_cliente/PedidoClienteApplication.java
@@ -0,0 +1,13 @@
+package tde3.pedido_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoClienteApplication.class, args);
+	}
+
+}
